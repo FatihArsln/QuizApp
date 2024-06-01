28f15e36c389c9afaa90fb9d5fa1f075df871dc3
Fatih Arslan <Fatih1106@live.de>
2024-06-01 18:25:24 +0200

    progress Bar

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ let questions = [
     },
     {
         "question": "Wie ist die grundlegende Struktur eines HTML-Dokuments?",
-        "answer_1": "<html><head><body></body></head></html>",
-        "answer_2": "<html><body><head></head></body></html>",
-        "answer_3": "<html><header><footer></footer></header></html>",
-        "answer_4": "<html><head></head><body></body></html>",
+        "answer_1": "html head body /body /head /html>",
+        "answer_2": "html body head /head /body /html",
+        "answer_3": "html header footer /footer /header /html>",
+        "answer_4": " html head /head body /body /html>",
         "right_answer": 3
     },
     {
-        "question": "Was ist der Zweck des <!DOCTYPE html> Deklaration?",
+        "question": "Was ist der Zweck des !DOCTYPE html Deklaration?",
         "answer_1": "Es definiert den Titel des Dokuments.",
         "answer_2": "Es legt die Dokumententyp-Definition fest.",
         "answer_3": "Es verlinkt ein externes Stylesheet.",
@@ -33,10 +33,10 @@ let questions = [
     },
     {
         "question": "Wie fügt man ein Bild in eine HTML-Seite ein?",
-        "answer_1": "<image src='bild.jpg'>",
-        "answer_2": "<img href='bild.jpg'>",
-        "answer_3": "<img src='bild.jpg'>",
-        "answer_4": "<image href='bild.jpg'>",
+        "answer_1": "'image src='bild.jpg",
+        "answer_2": "img href='bild.jpg'",
+        "answer_3": "img src='bild.jpg'",
+        "answer_4": "image href='bild.jpg'",
         "right_answer": 2
     },
     {
@@ -48,7 +48,7 @@ let questions = [
         "right_answer": 2
     },
     {
-        "question": "Was ist der Unterschied zwischen <div> und <span>?",
+        "question": "Was ist der Unterschied zwischen div und span?",
         "answer_1": "<div> ist ein Inline-Element, <span> ist ein Block-Level-Element.",
         "answer_2": "<div> ist ein Block-Level-Element, <span> ist ein Inline-Element.",
         "answer_3": "<div> ist ein semantisches Element, <span> nicht.",
@@ -57,15 +57,16 @@ let questions = [
     },
     {
         "question": "Wie bindet man ein externes Stylesheet in eine HTML-Datei ein?",
-        "answer_1": "<style src='style.css'>",
-        "answer_2": "<link rel='stylesheet' href='style.css'>",
-        "answer_3": "<stylesheet>style.css</stylesheet>",
-        "answer_4": "<css href='style.css'>",
+        "answer_1": "style src='style.css'",
+        "answer_2": "link rel='stylesheet' href='style.css'",
+        "answer_3": "stylesheet>style.css</stylesheet",
+        "answer_4": "css href='style.css'",
         "right_answer": 1
     }
 ];
 
 let currentQuestion = 0;
+let rightQuestions = 0;
 
 
 function init() {
@@ -88,19 +89,71 @@ function answer(selection) {
     console.log("Selection answer is " + selection); // selection ist die angelickte antwort
     let selectedQuestionNumber = selection.slice(-1); // holt den letzden Buchstaben wer aus dem text in HTML.
     console.log('selectedQuestionNumber is ', selectedQuestionNumber); // hier wird der letzde Buchstabe angezeigt
-    console.log('Current question is', question['right_answer']); // zeigt an welche antort richtig ist
+    console.log('Current question is', question['right_answer']); // zeigt an welche antwort richtig ist
 
 
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
     if (selectedQuestionNumber == question['right_answer']) { // hier wird der angeklickte  letzde Buchstabe  verglichen mit dem string in jason
         console.log('Richtig');
-        document.getElementById(selection).parentNode.classList.add('bg-success');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');// ändert die farbe des buttons nachdem eine antwort angeklickt wurde.
+        rightQuestions++;
     } else {
         console.log('Falsch');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('next-button').disabled = false;
+    document.getElementById('remove-button').disabled = false;
+
+}
+
+function nextQuestion() {
+    // Show End Screen
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionBody').style = 'display: none';
+
+        document.getElementById('amaountOfQuestions').innerHTML = questions.length;
+        document.getElementById('rightQuestion').innerHTML = rightQuestions;
+    } else { // Show question
+
+        let percent = (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+
+        document.getElementById('progressBar').innerHTML = `${percent}%`;
+        document.getElementById('progressBar').style = `width:${percent}%;`;
+
+        currentQuestion++;  // z.b von 0 auf 1
+        document.getElementById('next-button').disabled = true;
+        document.getElementById('remove-button').disabled = true;
+        resetClassListAnswer();
+        showQuestion();
+    }
+}
+
+function removeQuestion() {
+    currentQuestion--;
+    showQuestion();
+}
+
+function resetClassListAnswer() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame() {
+    
+    document.getElementById('endScreen').style = 'display: none';
+    document.getElementById('questionBody').style = '';
+    currentQuestion = 0;
+    rightQuestions = 0;
+    init();
 
 }
